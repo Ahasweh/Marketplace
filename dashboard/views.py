b55028b8567d5f1d944ac1c6a8569a3ac71160fb
ahmad <ahasweh@gmail.com>
2023-12-27 19:15:24 +0300

    Initial Commit

---
+++ dashboard/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from item.models import Item
+
+# Create your views here.
+
+
+@login_required
+def dashboard(request):
+    items= Item.objects.filter(created_by=request.user)
+    return render(request,'dashboard/dashboard.html',{
+        'title':'Dashboard',
+        'items':items,
+    })
+
+
+
